71cdc6eb27d53a64628b8eb5991a8c5cddef5f8d
서정호 | JH$EO <121909004+jhseo808@users.noreply.github.com>
2025-01-10 08:54:01 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import random
 
 def send_msg(my_msg, repeat_number):
     for i in range(int(repeat_number)):
-        time_wait = random.uniform(3, 5) # 랜덤값
+        time_wait = random.uniform(3, 5) # 랜덤값 생성
         print('Repeat Number : ', i + 1, end='')
         print(' // Time wait : ', time_wait)
         time.sleep(time_wait)
@@ -20,8 +20,7 @@ def send_msg(my_msg, repeat_number):
         pyautogui.keyDown('down')
 
 
-def filter_friend(filter_keyword, init_number):
-    # 사람 아이콘 클릭
+def filter_friend(filter_keyword, init_number): # 사람 아이콘 클릭
     try:
         click_img(img_path + 'person_icon.png')
         try:
@@ -30,14 +29,13 @@ def filter_friend(filter_keyword, init_number):
             print('e ', e)
     except Exception as e :
         print('e ', e)
-    # X 버튼이 존재한다면 클릭하여 내용 삭제
-    try:
+    try: # X 버튼 존재 시 클릭하여 내용 삭제
         click_img(img_path + 'x.png')
     except:
         pass
     time.sleep(1)
-    # 돋보기 아이콘 오른쪽 클릭
-    click_img_plus_x(img_path+'search_icon.png', 30)
+   
+    click_img_plus_x(img_path+'search_icon.png') # 돋보기 아이콘 클릭
     if filter_keyword == '':
         pyautogui.keyDown('esc')
     else:
@@ -47,24 +45,20 @@ def filter_friend(filter_keyword, init_number):
         pyautogui.keyDown('down')
     time.sleep(2)
 
-
 def click_img(imagePath):
     location = pyautogui.locateCenterOnScreen(imagePath, confidence = conf)
     x, y = location
     pyautogui.click(x, y)
 
-
-def click_img_plus_x(imagePath, pixel):
+def click_img_plus_x(imagePath, pixel=0):
     location = pyautogui.locateCenterOnScreen(imagePath, confidence = conf)
     x, y = location
     pyautogui.click(x + pixel, y)
-
 
 def doubleClickImg (imagePath):
     location = pyautogui.locateCenterOnScreen(imagePath, confidence = conf)
     x, y = location
     pyautogui.click(x, y, clicks=2)
-
 
 def set_delay():
     delay_time = input("몇 초 후에 프로그램을 실행하시겠습니까? : ")
@@ -76,7 +70,6 @@ def set_delay():
         time.sleep(1)
     sys.stdout.write("\r프로그램 실행!\n")
 
-
 def logout():
     try:
         click_img(img_path + 'menu.png')
@@ -87,17 +80,14 @@ def logout():
     except Exception as e:
         print('e ', e)
 
-
 def bye_msg():
     input('프로그램이 종료되었습니다.')
-
 
 def set_import_msg():
     with open("send_for_text.txt", "r", encoding='UTF-8') as f:
         text = f.read()
         print('======== 아래는 전송할 텍스트입니다. ========\n', text)
         return text
-
 
 def initialize():
     print('Monitor size : ', end='')
@@ -115,7 +105,7 @@ def initialize():
 
 # config
 img_path = os.path.dirname(os.path.realpath(__file__)) + '/img/'
-conf = 0.90
+conf = 0.80 # 0.90 이미지 추척안됨
 pyautogui.PAUSE = 0.5
 
 if __name__ == "__main__":
